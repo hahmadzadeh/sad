d4fa56bd40453ee3fe52ab11c29d8afd972c4f7e
Hossein Ahmadzadeh <h.ahmadzadeh1998@gmail.com>
2018-12-28 14:10:43 +0330

    Initial commit

---
+++ src/main/java/ir/sharif/sad/SadApplication.java
@@ -0,0 +1,14 @@
+package ir.sharif.sad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/ir/sharif/sad/SadApplicationTests.java
@@ -0,0 +1,17 @@
+package ir.sharif.sad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
