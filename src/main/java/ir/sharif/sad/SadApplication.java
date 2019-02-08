cf33f2f92d8761280f2dce0018f4a7d30414bbbf
Hossein Ahmadzadeh <h.ahmadzadeh1998@gmail.com>
2019-02-08 19:33:57 +0330

    fix template data

--- src/main/java/ir/sharif/sad/SadApplication.java
+++ src/main/java/ir/sharif/sad/SadApplication.java
@@ -1,12 +1,61 @@
 package ir.sharif.sad;
 
+import ir.sharif.sad.entity.Role;
+import ir.sharif.sad.entity.User;
+import ir.sharif.sad.enumerators.Roles;
+import ir.sharif.sad.repository.RoleRepository;
+import ir.sharif.sad.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
-public class SadApplication{
+public class SadApplication implements CommandLineRunner {
+	private final RoleRepository roleRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final UserRepository userRepository;
+
+	@Autowired
+	public SadApplication(RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository userRepository) {
+		this.roleRepository = roleRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+		this.userRepository = userRepository;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SadApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<Role> all = roleRepository.findAll();
+		if(all.isEmpty()){
+			roleRepository.save(new Role("ROLE_ADMIN"));
+			roleRepository.save(new Role("ROLE_FOUNDATION"));
+			roleRepository.save(new Role("ROLE_VOLUNTEER"));
+		}
+		User admin = userRepository.findByEmail("admin");
+		if(admin == null){
+			User user = new User();
+			user.setId(1);
+			user.setEmail("admin");
+			user.setName("admin");
+			user.setActive(1);
+			user.setLastName("admini");
+			Role userRole = roleRepository.findByRole(Roles.ADMIN.toString());
+			Set<Role> roleSet = new HashSet<>();
+			roleSet.add(userRole);
+			user.setRoles(roleSet);
+			user.setPassword(bCryptPasswordEncoder.encode("nimda"));
+			userRepository.saveAndFlush(user);
+		}
+
 	}
 }
 
--- src/main/java/ir/sharif/sad/entity/Role.java
+++ src/main/java/ir/sharif/sad/entity/Role.java
@@ -1,12 +1,14 @@
 package ir.sharif.sad.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "role")
+@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,4 +16,8 @@ public class Role {
     private int id;
     @Column(name = "role")
     private String role;
+
+    public Role(String role) {
+        this.role = role;
+    }
 }
