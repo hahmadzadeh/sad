9a3e345527f19b3c14b7ab4d345fab17d05b6cfa
Hossein Ahmadzadeh <h.ahmadzadeh1998@gmail.com>
2019-02-08 18:32:17 +0330

    add logger

--- src/main/java/ir/sharif/sad/configuration/CustomBasicAuthenticationEntryPoint.java
+++ src/main/java/ir/sharif/sad/configuration/CustomBasicAuthenticationEntryPoint.java
@@ -7,11 +7,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ir.sharif.sad.service.VolunteerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 public class CustomBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
-
+    private final Logger logger = LoggerFactory.getLogger(VolunteerService.class);
     @Override
     public void commence(final HttpServletRequest request,
                          final HttpServletResponse response,
@@ -19,7 +22,7 @@ public class CustomBasicAuthenticationEntryPoint extends BasicAuthenticationEntr
         //Authentication failed, send error response.
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName() + "");
-
+        logger.error(request.getRemoteUser());
         PrintWriter writer = response.getWriter();
         response.setStatus(401);
         writer.println(authException.getMessage());
