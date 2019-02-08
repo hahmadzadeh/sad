f676b7bd9c372b71f9893a1311edf1e797319633
Hossein Ahmadzadeh <h.ahmadzadeh1998@gmail.com>
2019-02-08 18:21:39 +0330

    fix bugs off hero i guess?

--- src/main/java/ir/sharif/sad/configuration/SecurityConfiguration.java
+++ src/main/java/ir/sharif/sad/configuration/SecurityConfiguration.java
@@ -68,7 +68,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web){
         web.ignoring().antMatchers("/register/**");
     }
 }
